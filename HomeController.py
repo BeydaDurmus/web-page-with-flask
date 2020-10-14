fef4470cc6501a371a5f10b20813f96a65c804fd
BeydaDurmus <beydadurmus8@gmail.com>
2020-10-14 23:17:29 +0300

    Template Tamamlandı.

--- HomeController.py
+++ HomeController.py
@@ -3,14 +3,19 @@ from flask import jsonify,request,render_template
 import requests
 from werkzeug.utils import secure_filename
 import os,base64
-
+import ast
 from ps_manager import CloudStorageUploader
+from PIL import Image 
+import io
+import urllib.request
 
 
 YUKLEME_KLASORU = 'static/yuklemeler'
 UZANTILAR = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-
-app = Flask(__name__,static_url_path="/static")
+response = None
+# app = Flask(__name__,static_url_path="/static")
+app=Flask(__name__)
+app.static_folder='static'
 app.config['UPLOAD_FOLDER'] = YUKLEME_KLASORU
 
 
@@ -23,14 +28,15 @@ def uzanti_kontrol(dosyaadi):
 #############################################################
 
 
-@app.route('/mainPage',methods=['GET'])
-def static_file1():
-    return app.send_static_file('index.html')
+@app.route('/tydetail',methods=['GET'])
+def GetProductPage():
+    return render_template('single-product.html')
 
 
 @app.route('/detail',methods=['GET','POST'])
-def static_file():
-    if request.method=='POST':
+def product_detail():
+   global response
+   if request.method=='POST':
       if 'uploadFile' not in request.files:
          return app.send_static_file('product_detail3.html')     
 
@@ -45,9 +51,31 @@ def static_file():
       cloud_manager=CloudStorageUploader('main-product-set-images')
       cloud_manager.UploadToBucket('api-imgs/deneme.jpg',YUKLEME_KLASORU+'/'+uploadFile.filename)
       response_api=requests.post(url="http://127.0.0.1:3005//api/v1/PostVision/")
-      print(response_api.content)
+      if not response_api is None:
+         response=response_api.content
+         byte_response=response_api.content
+         dict_str=byte_response.decode("UTF-8")
+         response=ast.literal_eval(dict_str)
+         url=response['image_url']
+         with urllib.request.urlopen(url) as url:
+            f = io.BytesIO(url.read())
+
+         img = Image.open(f)
+
+         # image=requests.get()
+         # image=Image.open(BytesIO(image.content))
+         labels=response['labels']['values']
+         product_info=response['product_info']
+
+         print(labels)
+         return render_template('deneme.html',response=response,labels=labels,product_info=product_info,image=img)
+      else:
+         response=None
+         print(response)
+   if request.method=='GET':
+      return render_template('deneme.html')
       
-    return app.send_static_file('product_detail3.html',response=response_api.content)
+   return render_template('deneme.html')
 
 if __name__=='__main__':
     app.run(port=3003,debug=True)
